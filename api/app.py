b0bab820f7bafeaaf659a10ce37b202300c42e70
mdb571 <r.mohammedbilal@gmail.com>
2020-12-26 14:40:16 +0530

    api deploy

--- api/app.py
+++ api/app.py
@@ -2,9 +2,13 @@ from flask import Flask, request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+from flask_cors import CORS
+from image import get_image_url
+
 
 app=Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
@@ -13,26 +17,60 @@ db= SQLAlchemy(app)
 ma =Marshmallow(app)
 
 class Gift(db.Model):
-    username=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(100),primary_key=True)
     description=db.Column(db.String(200))
+    url=db.Column(db.String)
     price = db.Column(db.Float)
     balance = db.Column(db.Float)
 
-    def __init__(self,username,description,price,balance):
+    def __init__(self,username,description,url,price,balance):
         self.username=username
         self.description=description
+        self.url=get_image_url(url)
         self.price=price
         self.balance=balance
 
 class GiftSchema(ma.Schema):
     class Meta:
-        fields = ('username','description','price','balance')
+        fields = ('username','description','url','price','balance')
 
 
 
 gift_schema = GiftSchema()
 gifts_schema = GiftSchema(many=True)
 
+@app.route('/gift',methods=['POST'])
+def add_gift():
+  username = request.json['username']
+  description = request.json['description']
+  url=request.json['url']
+  price = request.json['price']
+
+  new_gift = Gift(username, description,url, price, price)
+
+  db.session.add(new_gift)
+
+  db.session.commit()
+
+  return gift_schema.jsonify(new_gift)
+
+
+@app.route('/all', methods=['GET'])
+def get_products():
+  all_gifts = Gift.query.all()
+  result = gifts_schema.dump(all_gifts)
+  return jsonify(result)
+
+
+@app.route('/<username>/pay/<pay>',methods=['GET'])
+def pay(username,pay):
+    gift=Gift.query.get(username)
+
+    gift.balance=gift.price-int(pay)
+
+    db.session.commit()
+
+    return gift_schema.jsonify(gift)
 
 
 if __name__ == '__main__':
---
+++ api/image.py
@@ -0,0 +1,30 @@
+import requests
+import os
+
+
+def get_image_url(url):
+    search_term=url.split('/')[3]
+    search_term=search_term.replace('-',' ')
+    print(search_term)
+    subscription_key = os.environ.get('API_KEY')
+    search_url = "https://api.bing.microsoft.com/v7.0/images/search"
+    
+
+    headers = {"Ocp-Apim-Subscription-Key" : subscription_key}
+    params  = {"q": search_term, "license": "public", "imageType": "photo"}
+
+    response = requests.get(search_url, headers=headers, params=params)
+    response.raise_for_status()
+    search_results = response.json()
+    if search_results["value"] == []:
+        search_term=search_term.split()[0]
+        params  = {"q": search_term, "license": "public", "imageType": "photo"}
+        response = requests.get(search_url, headers=headers, params=params)
+        response.raise_for_status()
+        search_results = response.json()
+
+    thumbnail_urls = search_results["value"][0]["contentUrl"]
+
+    return(thumbnail_urls)
+
+
