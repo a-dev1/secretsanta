49bfc1f9a5a986516ee99060e40aeeef0dd5f505
code-shoot <franksmithattheshoot@gmail.com>
2020-12-26 08:22:36 +0530

    adding cover element for cards

---
+++ script.js
@@ -0,0 +1,21 @@
+let data = [
+  {
+    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
+  },
+  {
+    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
+  },
+  {
+    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
+  },
+  {
+    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
+  },
+  {
+    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
+  },
+  {
+    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
+  }
+]
+
