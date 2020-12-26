980d9bc1b1200499ed211c753162eef0b664d79a
code-shoot <franksmithattheshoot@gmail.com>
2020-12-27 01:44:09 +0530

    adding cards from api

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-let data = [
-  {
-    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
-  },
-  {
-    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
-  },
-  {
-    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
-  },
-  {
-    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
-  },
-  {
-    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
-  },
-  {
-    reason: '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus beatae dolores soluta porro distinctio placeat, cupiditate voluptate earum omnis culpa deserunt sit atque veniam expedita, ea neque corporis exercitationem nesciunt maiores sed adipisci! Provident inventore voluptas ad quasi optio tempora.'
-  }
-]
+let cards = document.querySelector('.cards');
 
-let detail_button = document.querySelector('.show-details');
-let close_details = document.querySelector('.close-cover');
-
-const showDetains = () => {
+const showDetains = (e) => {
   console.log('show details');
-  detail_button.parentElement.parentElement.lastElementChild.style.display = 'block';
+  e.target.parentElement.parentElement.lastElementChild.style.display = 'block';
 }
 
-const hideDetails = () => {
-  console.log('hidde details')
-  close_details.parentElement.parentElement.parentElement.style.display = 'none';
+const hideDetails = (e) => {
+  console.log('hide details');
+  e.target.parentElement.parentElement.parentElement.style.display = 'none';
 }
-detail_button.addEventListener('click', showDetains);
+
+let createCard = (item) => {
+  let card_container = document.createElement('div')
+  card_container.classList.add('card-container')
+  
+  card_container.innerHTML = `
+  <div class="card-header">
+  <img src="${item.url}" alt="product">
+  <h2 class="name">${item.username}</h2>
+  </div>
+  <div class="card-footer">
+  <span>${item.giftname}</span>
+  <span>Need: $${item.balance}</span>
+  </div>
+  <div class="cover-button">
+  <button class="show-details" onclick="showDetains(event)">GIFT</button>
+  </div>
+  
+  <div class="deadly-nested">
+  <div class="details-cover">
+  
+  <div class="details-container">
+  <div class="details-img-container">
+  <img src="${item.url}" alt="">
+  </div>
+  <div class="details">
+  <h1>${item.giftname}</h1>
+  <h3>${item.description}.</p>
+  <span>Need: $${item.balance}</span>
+  <button type="submit">PAY</button>
+  </div>
+  <button class="close-cover" onclick="hideDetails(event)">X</button>
+  </div>
+  
+  </div>
+  </div>
+  `;
+
+  cards.appendChild(card_container);
+  console.log('adding a new card');
+}
+
+
+const showCards = () => {
+  fetch('https://holidayhacks.herokuapp.com/all')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(item => createCard(item))
+    console.log(data)
+  })
+  .catch((e) => console.log(e));
+}
+
+// document.querySelectorAll('.show-details').forEach(button => {
+//   button.addEventListener('click', showDetains);
+// })
+
