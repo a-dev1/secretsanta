be03b676d745980cdd0fe69ea6e922bb8b2f3828
code-shoot <franksmithattheshoot@gmail.com>
2020-12-27 16:01:22 +0530

    adding shadown to submit page

--- script.js
+++ script.js
@@ -90,20 +90,3 @@ const showCards = () => {
 
 // const payForm = document.querySelectorAll('.payForm');
 
-
-
-
-// FORM
-// const thisForm = document.getElementById('myForm');
-// thisForm.addEventListener('submit', async function (e) {
-//     e.preventDefault();
-//     const formData = new FormData(thisForm).entries()
-//     const response = await fetch('https://holidayhacks.herokuapp.com/gift', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(Object.fromEntries(formData))
-//     });
-
-//     const result = await response.json();
-//     console.log(result)
-// });
